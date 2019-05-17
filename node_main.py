4751c65d90809875924e42857f69e9a71f872e58
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-05-17 18:51:17 +0200

    remove printed todo

--- node_main.py
+++ node_main.py
@@ -1,4 +1,4 @@
 def main(peer_nids, my_nid, send, receive, storage):
-    print('todo: implement paxos')
+    # TODO: implement this.
     print(f'peer_nids are: {peer_nids}')
     print(f'my_nid is: {my_nid}')
